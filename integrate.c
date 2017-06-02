8d74cdcef35ca28f9443efaf5d9f695c82be2f56
Miguel Vesga <miguelvesga@outlook.com>
2017-06-02 17:49:40 -0500

    Somehow the wrong implementation managed to work; fixed.

--- integrate.c
+++ integrate.c
@@ -1,23 +1,23 @@
 #include <math.h>
 #include <stdio.h>
 
-#define n 10000000.0
+#define n 1e7
 
 double riemann(double(*f)(double), double a, double b) {
 	double val = 0.0;
-	for (double i = 0; i <= n; i += 1.0) {
-		double h = (a + (fabsf(b - a) * (i / n)));
+	for (int i = 0; i <= n; i++) {
 		double w = (fabsf(b - a) / n);
-		val += f(h) + w;
+		double h = (a + (fabsf(b - a) * (i / n)));
+		val += f(h) * w;
 	}
-	return (val / n);
+	return val;
 }
 
 double f(double x) {
 	return log(x);
 }
 
-double main(void) {
+int main(void) {
 	double r = riemann(&f, 1.0, 2.0);
 	printf("%.5f\n", r);
 }
